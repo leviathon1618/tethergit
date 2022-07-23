d4a833943cd9328b9fb9c486559e3937e146e16a
leviathon1432 <levidelaney2000@gmail.com>
2022-07-23 18:10:36 +1200

    ship stuff

---
+++ Assets/scripts/homebase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homebase : MonoBehaviour
+{
+    public int progress = 0;
+    public List<GameObject> ship_list = new List<GameObject>();
+    public List<bool> parts_found = new List<bool>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
