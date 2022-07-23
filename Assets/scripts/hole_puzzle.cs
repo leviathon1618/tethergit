df0d0e492b097b3c08563c4a0ebb8db8e9e584fa
leviathon1432 <levidelaney2000@gmail.com>
2022-07-23 17:25:17 +1200

    puzzle fix

--- Assets/scripts/hole_puzzle.cs
+++ Assets/scripts/hole_puzzle.cs
@@ -10,10 +10,10 @@ public class hole_puzzle : MonoBehaviour
     public GameObject bridge_1;
     public float distance;
 
-
-    public IEnumerator OnTriggerEnter2D(Collider2D collision)
+    public IEnumerator OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.name == "bridge_1") {
+        if (collision.transform.name == "bridge_1")
+        {
             collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
             print("Collider Disabled");
             for (float i = 0; i <= 1; i += 0.1f)
@@ -25,8 +25,9 @@ public class hole_puzzle : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
             collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
         }
-
     }
+
+
 
     // Start is called before the first frame update
     void Start()
